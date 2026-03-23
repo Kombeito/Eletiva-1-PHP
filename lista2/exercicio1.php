18ce6be0355ba325db57ddc1c221c7a735d3edd5
Kombeito <cardosoalec123@gmail.com>
2026-03-23 20:04:04 -0300

    olhaai

--- lista2/exercicio1.php
+++ lista2/exercicio1.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <div class="container">
-<h1>Lista 1 - exercício 1:</h1>
+<h1>Lista 1 - exercício 1</h1>
 <form method="post">
 <div class="mb-3">
               <label for="num1" class="form-label">Digite um número:</label>
