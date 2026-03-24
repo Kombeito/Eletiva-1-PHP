106cad438c6445a6b0ea4dc2f0a791a7b5f155ed
Kombeito <cardosoalec123@gmail.com>
2026-03-23 21:07:53 -0300

    exercicio10

--- lista2/exercicio8.php
+++ lista2/exercicio8.php
@@ -3,12 +3,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 7 - Lista 1</title>
+<title>Exercício 8 - Lista 1</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body>
 <div class="container">
-<h1>Lista 1 - exercício 7</h1>
+<h1>Lista 1 - exercício 8</h1>
 <form method="post">
 <div class="mb-3">
               <label for="altura" class="form-label">Digite a Altura:</label>
