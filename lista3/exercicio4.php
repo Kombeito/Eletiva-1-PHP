6f787ee7f0f9b9ad2514547ce4aa620c306098f2
Kombeito <cardosoalec123@gmail.com>
2026-03-24 21:56:50 -0300

    exercicio 4 e correções

--- lista3/exercicio4.php
+++ lista3/exercicio4.php
@@ -3,38 +3,30 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 3 da lista 2</title>
+<title>Exercício 4 da lista 2</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container">
-<h1>Lista 2 - Exercicio 3</h1>
+<h1>Lista 2 - Exercicio 4</h1>
 <form method="post">
   <div class="mb-3">
     <label for="num1" class="form-label">Digite um número:</label>
-    <input type="number" id="num1" name="num1" class="form-control" required>
-  </div>
-  <div class="mb-3">
-    <label for="num2" class="form-label">Digite outro número:</label>
-    <input type="number" id="num2" name="num2" class="form-control" required>
+    <input type="number" id="num1" step="0.01" name="num1" class="form-control" required>
   </div>
   <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $a = (int)$_POST['num1'];
-    $b = (int)$_POST['num2'];
-    $soma = $a + $b;
+    $preco = (int)$_POST['num1'];
     
-    if ($a < $b){
-        echo "<p>$A . $b</p>";
-    }
-    else if ($B < $A) {
-        echo "<p>$B . $A</p>";
-    }
-    else {
-        echo "<p>Os números são iguais, sendo eles: $A</p>";
+    if ($preco > 100){
+        $valordescontado = $preco - ($preco * (15 / 100));
+        $numero_formatado = number_format($valordescontado, 2, ',', '.');
+        echo "<p> O valor do produto com desconto fica: R$$numero_formatado </p>";
+    } else{
+        echo "<p>O preço do produto é menor que R$100,00. Então continua sendo" . $preco . "<p>";
     }
 }
 ?>
