ed5e271b4a3bbc32a93471cf0f0ba8f82e18cd91
Kombeito <cardosoalec123@gmail.com>
2026-03-26 20:46:44 -0300

    exercicio 3 lista 4

--- lista4/exercicio3.php
+++ lista4/exercicio3.php
@@ -3,25 +3,33 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 2 da lista 4</title>
+<title>Exercício 3 da lista 4</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body>
 <div class="container">
-<h1>Lista 4 - Exercício 2</h1>
+<h1>Lista 4 - Exercício 3</h1>
 <form method="post">
 <div class="mb-3">
     <label for="palavra" class="form-label">Digite uma palavra:</label>
     <input type="text" id="palavra" name="palavra" class="form-control" required>
 </div>
+<div class="mb-3">
+    <label for="palavra2" class="form-label">Digite outra palavra:</label>
+    <input type="text" id="palavra2" name="palavra2" class="form-control" required>
+</div>
 
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $palavra = $_POST['palavra'];
-        $maiusc = strtoupper($palavra);
-        $minusc = strtolower($palavra);
-        echo"<p>Maiúsculo: $maiusc</p>";
-        echo"<p>Minúsculo: $minusc</p>";
+        $palavra2 = $_POST['palavra2'];
+
+        if (strpos($palavra, $palavra2) !== false){
+            echo"<p>A palavra '$palavra2' está contida em '$palavra'.</p>";
+        } else {
+        echo"<p>A palavra '$palavra2' NÃO está contida em '$palavra'.</p>";
+
+        }
 }
 ?>
 
