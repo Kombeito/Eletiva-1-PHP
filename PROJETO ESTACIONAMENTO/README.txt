e6929b48d8eb737200f301033b45c3b5b4b71552
Kombeito <cardosoalec123@gmail.com>
2026-05-26 20:29:40 -0300

    começando o projeto - enviando começo

--- PROJETO ESTACIONAMENTO/README.txt
+++ PROJETO ESTACIONAMENTO/README.txt
@@ -1,1 +1,15 @@
-Oi projetooooo afff - atrasado em mds
+Sistema de Controle de Estacionamento
+Alec Bryan Cardoso e Silva
+
+Pode utilizar esse Login:
+Email: adm@adm
+Senha: 123
+
+como deve ser:
+
+Funções:
+- CRUD de clientes
+- CRUD de veículos
+- CRUD de vagas
+- Registro de entrada e saída de veículos
+- Relacionamento entre cliente, veículo e vaga no registro de movimento
---
+++ PROJETO ESTACIONAMENTO/index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login - Estacionamento</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/estilo.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+<div class="container login-area d-flex justify-content-center align-items-center">
+  <div class="card login-card shadow p-4" style="width: 100%; max-width: 430px;">
+    <h3 class="text-center mb-4">Sistema de Controle de Estacionamento</h3>
+    <p class="text-center text-muted">Alec Bryan Cardoso e Silva</p>
+    <form method="post">
+      <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input name="email" type="email" class="form-control" placeholder="Digite seu email" required>
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Senha</label>
+        <input name="senha" type="password" class="form-control" placeholder="Digite sua senha" required>
+      </div>
+
+      <button type="submit" class="btn btn-primary w-100">Entrar</button>
+    </form>
+
+    <?php
+      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        if($email == "adm@adm" && $senha == '123'){
+          $_SESSION['nome'] = 'Alec Bryan';
+          $_SESSION['acesso'] = true;
+          header('Location: principal.php');
+          exit();
+        } else {
+          $_SESSION['acesso'] = false;
+          echo "<p class='text-danger mt-3'>Email e/ou senha incorretos!</p>";
+        }
+      }
+?>
+
+    <p class="text-center mt-3 mb-0">
+      Usuário: adm@adm | Senha: 123
+    </p>
+  </div>
+</div>
+
+</body>
+</html>
